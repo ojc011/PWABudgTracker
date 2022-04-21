2011942428b51e5f6d355cb70c4116ff94698124
Oliver C <ocronk10@gmail.com>
2022-04-20 20:11:29 -0500

    intercepts fetch requests if avail

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,3 +44,19 @@ self.addEventListener('activate', function (e) {
         })
     )
 });
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // responds with cachce if available
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {      // tries fetching request if there is no cache
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+
+        })
+    )
+});
